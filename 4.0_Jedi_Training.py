8140919017bf48c56a9a4436770f9cd63c8d2196
reischeljon <reischeljon@urbandaleschools.com>
2024-01-22 09:04:19 -0600

    10 lines

--- 4.0_Jedi_Training.py
+++ 4.0_Jedi_Training.py
@@ -8,7 +8,9 @@
          print("You have serious Jedi potential")
      elif:
          print("Jedi, you will never be.")
-
+#line 6 and 9 no indentation needed while 7 and 10 only need one
+#missing : on line 7
+#line 9 elif statment should include elif midichlorians < 1000:
 
  # 2. Make the following program work. (3 mistakes)  (3pts)
      
@@ -31,8 +33,9 @@
      x = input("Name one of the top 3 greatest Jedi.")
      if jedi == Yoda or Luke Skywalker or Obi-Wan Kenobi:
          print "That is correct!"
-
-
+# when defining yoda luke skywalker or obi-wan kenobi include "" between the names
+# if statement deos not work because jedi was never defined change x to jedi.
+# when printing text needs to include ()
 
  # 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
  #    Print "Not a choice!" if they don't choose any of the three and set sensitivity to blank text.  (6pts)
@@ -88,5 +91,21 @@ GRADING 2.0    (10pts)
 Copy your Grading 1.0 program below and then modify it to also print out the letter grade depending on the numerical grade.
 If they fail, tell them to "Transfer to Johnston!"
 '''
+semester_grade = float(input("Enter your semester grade:"))
+final_exam = float(input("Enter you exam grade:"))
+final_worth = float(input("Enter the final exam worth:"))
 
+fworth=final_worth/100
+sgworth = 1-fworth
+og = fworth*final_exam+sgworth*semester_grade
+if og >= 90:
+    print("A")
+if og >= 80:
+    print("B")
+if og >= 70:
+    print("C")
+if og >= 60:
+    print("D")
+if og >= 50:
+    print("Transfer to Johnston!")
 
--- Quiz_Master_Project.py
+++ Quiz_Master_Project.py
@@ -4,3 +4,92 @@ QUIZ MASTER PROJECT  (4pts)
 The criteria for the project are on the website. Make sure you test this quiz with 
 two of your student colleagues before you run it by your instructor.
 '''
+total_questions =0
+total = 0
+correct = ("Correct")
+incorrect = ("Incorrect")
+
+question1 = input("where is Adventure land located? ")
+total_questions += 1
+if question1 == "Iowa" or question1 == "IOWA" or question1 == "IA" or question1 == "iowa":
+    print(correct)
+    total += 1
+else:
+    print(incorrect)
+    total += 0
+
+question2 = input("What is the name of Adventure land's mascot ")
+total_questions += 1
+if question2 == "Bernie" or question2 == "bernie" or question2 == "Bernie Bernard" or question2 == "bernie bernard":
+    print(correct)
+    total += 1
+else:
+    print(incorrect)
+    total += 0
+
+question3 = input("how tall is the Storm Chaser in feet? ")
+total_questions += 1
+if question3 == "260" or question3 == "260 feet" or question3 == "260ft":
+    print(correct)
+    total += 1
+else:
+    print(incorrect)
+    total += 0
+
+question4 = input("What year did Adventure land introduce there newest event Phantom Fall Fest ")
+total_questions += 1
+if question4 == "2022":
+    print(correct)
+    total += 1
+else:
+    print(incorrect)
+    total += 0
+
+question5 = input("What new roller coaster was built this year at Adventure land? ")
+total_questions += 1
+if question5 == "flying viking" or question5 == "Flying Viking" or question5 == "Flying viking":
+    print(correct)
+    total += 1
+else:
+    print(incorrect)
+    total += 0
+
+question6 = input("What year was the Monster roller coaster first open to the public? ")
+total_questions += 1
+if question6 == "2016":
+    print(correct)
+    total += 1
+else:
+    print(incorrect)
+    total += 0
+
+question7 = input("What year did Adventureland open? \n A: 1973 \n B: 1974 \n C: 1975 \n D: I dont know \n Enter an answer:")
+total_questions += 1
+if question7 == "B" or question7 == "b":
+    print(correct)
+    total += 1
+elif question7:
+    print(incorrect)
+    total += 0
+
+
+grade = total/total_questions * 100
+if grade >= 90:
+    print(grade)
+    print("You got a A")
+elif grade >= 80:
+    print(grade)
+    print("You got a B")
+elif grade >= 70:
+    print(grade)
+    print("You got a C")
+elif grade >= 60:
+    print(grade)
+    print("You got a D")
+elif grade >= 50 or grade <= 0:
+    print(grade)
+    print("You got a F")
+
+
+
+
